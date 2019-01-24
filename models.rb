d83359649776f262f0e921283f57bfdd6540215e
Mohamed Jalloh <mohamedjalloh@Mohameds-MacBook-Air.local>
2019-01-24 15:00:19 -0500

    added a config.ru file

--- models.rb
+++ models.rb
@@ -3,6 +3,15 @@ require 'pg'
 
 set :database, 'postgresql:socialmedia'
 
+configure :development do
+    set :database, 'postgresql:socialmedia'
+  end
+  
+  # on heroku
+  configure :production do
+    set :database, ENV['DATABASE_URL']
+  end
+
 
 class User < ActiveRecord::Base
     has_many :posts, dependent: :destroy
